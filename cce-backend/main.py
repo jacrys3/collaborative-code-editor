9d92649e0ef355869c8ed3175141752c723fdc04
Jacrys3 <ryse0528@gmail.com>
2023-10-18 02:59:32 -0700

    Add users table and create user models

--- cce-backend/main.py
+++ cce-backend/main.py
@@ -1,7 +1,81 @@
+import os
+import databases
+import sqlalchemy
+from typing import List
 from fastapi import FastAPI
+from pydantic import BaseModel
+from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+database = databases.Database(DATABASE_URL)
+
+metadata = sqlalchemy.MetaData()
+
+engine = sqlalchemy.create_engine(
+    DATABASE_URL
+)
+
+users = sqlalchemy.Table(
+    "users",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("username", sqlalchemy.String),
+    sqlalchemy.Column("password", sqlalchemy.String)
+)
+
+class UserIn(BaseModel):
+    username: str
+    password: str
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    password: str
+
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World!"}
+
+@app.get("/users", response_model=List[User])
+async def get_users():
+    query = users.select()
+    return await database.fetch_all(query)
+
+@app.post("/users", response_model=User)
+async def create_user(user: UserIn):
+    print(user)
+    query = users.insert().values(username=user.username, password=user.password)
+    last_record_id = await database.execute(query)
+    return {**user.model_dump(), "id": last_record_id}
